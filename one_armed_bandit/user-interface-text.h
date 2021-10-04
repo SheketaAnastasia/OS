39074b44567e9565e921632b9c5b043d8b6a9241
SheketaAnastasia <ruzhuk.31@gmail.com>
2021-10-04 20:21:10 +0300

    add one armed bandit

---
+++ one_armed_bandit/user-interface-text.h
@@ -0,0 +1,11 @@
+// Input data messages
+void printEnterBudget();
+
+// Game messages
+void printYouWinX(int number);
+void printReturnYourBet();
+void printDoYouWantContinue();
+void printYouLose();
+void printYourCurrentBudget(int budget);
+void printGameOver();
+void printEnterYourBet();
